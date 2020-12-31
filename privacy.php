a14e9519c044a2351a65ca1b068766ec2b13b047
scotlanddebtrelief <76813056+scotlanddebtrelief@users.noreply.github.com>
2020-12-31 13:20:04 +0100

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>Privacy Policy</title>
+
+  
+
+</head>
+<body>
+<center>
+<table style="font-family: Arial; color: rgb(102, 102, 102); width: 707px; height: 88px; text-align: left; margin-left: auto; margin-right: auto;" cellspacing="10" cellpadding="10" border="0">
+  <tbody>
+
+    <tr align="justify">
+      <td>
+	  
+	  
+<p><strong>Privacy Policy</strong></p>
+
+        <p>We take our ethical responsibilities and your privacy 
+seriously. We   have a strong commitment to providing excellent service 
+to all our   customers and visitors of this Web Site, including 
+respecting your   concerns about privacy. This Privacy Policy discloses 
+how we collect,   protect, use and share information gathered about you 
+on our website. If   you use this site you explicitly agree to the terms
+ and conditions of   the Privacy Policy in effect at the time of your 
+use. We hope that this   disclosure will help increase your confidence 
+in our website and enhance   your experience on the Internet. Therefore,
+ in compliance with industry   standards and regulations enforced 
+typically by the Federal and   Provincial Governments, we abide the 
+following privacy policy.</p>
+        <p><strong>Browsing</strong></p>
+        <p>This website does not collect personally identifiable 
+information   from your computer when you browse this website and 
+request pages from   our servers. This means that, unless you 
+voluntarily and knowingly   provide us with personally identifiable 
+information, we will not know   your name, your email address, or any 
+other personally identifiable   information. We may use IP addresses, 
+browser types and access times to   analyze trends, administer the site,
+ improve site performance and gather   broad demographic information for
+ aggregate use. When you request a   page from our website, our servers 
+log the information provided in the   HTTP request header including the 
+IP number, the time of the request,   the URL of your request, and other
+ information that is provided in the   HTTP header. We collect the HTTP 
+request header information in order to   make our website function 
+correctly and provide you the functionality   that you see on this 
+website. We also use this information to better   understand how 
+visitors use our website and how we can better tune it,   its contents 
+and functionality to meet your needs. </p>
+        <p><strong>Information We Collect and Its Uses</strong></p>
+        <p>We collect your personal information if you decide to 
+purchase one of   our products or retain our services, participate in 
+our affiliate   marketing program, subscribe to our newsletter, complete
+ an application   form, participate in one of our surveys or transact 
+other business with   us. We need to collect personally identifiable 
+information from you to   execute the requested transaction, provide you
+ with a particular   service, and/or to further enhance your account. At
+ anytime, we may ask   you to voluntarily supply us with additional 
+information needed. We will   ask you for information such as, but not 
+limited to: name, current   and/or billing address, your e-mail address,
+ telephone number and, if   you purchase one of our products or 
+services, a valid credit card   number, your Social Security number and 
+certain other personal   information, such as your date of birth, 
+address, employment   information, and certain credit card and loan 
+account information. We   may use your email address to send a 
+confirmation and, if necessary, we   might use the other information to 
+contact you for help in processing.   In addition, when you provide 
+contact details for transaction requests   such as scheduling an 
+appointment or requesting a proposal, we will use   the contact 
+information to keep you updated about future offers or   promotions 
+unless you opt-out online or otherwise notify us. We may also   use the 
+information we collect about you in order to, but not limited   to: </p>
+        <p>• learn more about your interest in the products or services we offer and provide you with information; </p>
+        <p>• enroll customers who desire our services </p>
+        <p>• open customer files and establish their accounts </p>
+        <p>• provide customer service </p>
+        <p>• negotiate settlement of our customers’ debts (according to the terms and conditions of their written agreements) </p>
+        <p>• learn how to improve our products or services </p>
+        <p>• provide opportunities for our affiliates and other 
+companies to   inform you about the products or services they offer that
+ may interest   you </p>
+        <p>• to share aggregated statistical data with our business 
+partners or   for public relations. For example, we may disclose that a 
+specific   percentage of our users are between the ages of 25 and 35. </p>
+        <p>Aside from the ways mentioned above, we may use your 
+personally   identifiable information in many other ways, including 
+sending you   promotional materials, and sharing your information with 
+third parties   so that these third parties can send you promotional 
+materials. (By   "promotional materials," we mean communications that 
+directly promote   the use of Web sites, or the purchase of products or 
+services.).   However, you may "opt-out" of certain uses of your 
+personal information.</p>
+
+        <p><strong>Disclosure of Information to Third Parties</strong></p>
+		
+		<p>We may disclose a consumer's personally identifiable information in
+ order to effect or carry out any transaction that you have requested of
+ us or as necessary to complete our contractual obligations with you. We
+ may be compensated for this disclosure by our third-party partners. WE 
+RESERVE THE RIGHT TO SELL, RENT OR TRANSFER YOUR PERSONAL INFORMATION TO
+ THIRD PARTIES FOR ANY PURPOSE IN OUR SOLE DISCRETION. These companies 
+may employ the use of an Automated Dialing-Announcing Device (ADAD) to 
+make contact with you. We prohibit the sale or transfer of personal 
+information to non-affiliated entities for their use without giving you 
+the opportunity to opt-out (See Opt-Out below). We may share your 
+personally identifiable information with affiliated companies that are 
+directly or indirectly controlled by, or under common control of Debt 
+Advisors. We may send personally identifiable information about you to 
+non-affiliated companies that are not directly or indirectly controlled 
+by, or under common control of, Debt Advisors. The personal information 
+collected on this site and by third parties will be used to operate the 
+site and to provide the services or products or carry out the 
+transactions you have requested or authorized. We may change or broaden 
+the use of your personal information at any time. We and/or our 
+third-party partners may use your personal information to provide 
+promotional offers to you by means of email advertising, telephone 
+marketing, use of an Automated Dialing-Announcing Device (ADAD), direct 
+mail marketing, banner advertising, text messages at the phone number(s)
+ you provide, and other possible uses. You can, however, have your name 
+put on our do-not-share list by, writing to us at the email address 
+listed below.</p>
+		
+
+        <p><strong>Choice/Opt-out</strong></p>
+        <p>We provide you the opportunity to ‘opt-out’ of having your 
+personally   identifiable information used for certain purposes, when we
+ ask for   this information. For example, if you purchase a 
+product/service but do   not wish to receive any additional marketing 
+material from us, you can   indicate your preference on our order form. 
+If you no longer wish to   receive our newsletter and promotional 
+communications, you may opt-out   of receiving them by following the 
+instructions included in each   newsletter or communication or by 
+emailing or calling us per the   information contained on our contact 
+page. If you do not wish to have   your personal information collected 
+by any third party that is not our   agent/service provider, please 
+contact our customer service department   to actively opt-out of having 
+your personal information shared. Customer   Service Contact 
+Information: <br>
+          <br>
+          Debt Advisors<br>
+          Email: support@debtadvisors22.ca </p>
+        <p><strong>Service Providers</strong></p>
+        <p>We may use other third parties to provide certain clerical 
+and   information processing and shipping services on our site. When you
+ sign   up for our services, we will share only as much information as 
+is   necessary for the provision of those services. These third parties 
+are   prohibited from using your personally identifiable information for
+ any   other purpose.</p>
+        <p><strong>Securing the Transmission and Storage of Information</strong></p>
+        <p>The security of your personal information is extremely 
+important to   us. We employ generally accepted industry standards to 
+protect the   personal information submitted to us, both during 
+transmission and once   we receive it. Our site uses SSL (Secure Sockets
+ Layer) security   technology to encrypt information you provide to us 
+through the site   when entering sensitive information (such as credit 
+card number and/or   social security number) on our registration or 
+order forms. This ensures   that your information is encrypted as it 
+travels over the Internet. SSL   is an industry-standard protocol for 
+encryption over the internet.   After information reaches 
+Debt Advisors, it is stored on a secure server   that resides 
+behind firewalls designed to block unauthorized access from   outside of
+ Debt Advisors . You can help to maintain the security of your   
+online transactions by not sharing your personal information or password
+   with anyone. Remember, no method of transmission over the Internet, 
+or   method of electronic storage, is 100% secure. (Example: any 
+information   you provide us by email is not encrypted) Therefore, while
+ we strive to   use commercially acceptable means to protect your 
+personal information,   we cannot guarantee its absolute security. If 
+you have any questions   about the security on our Web site, please feel
+ free to e-mail us.</p>
+        <p><strong>Log Files</strong></p>
+        <p>As is true of most Web sites, we gather certain information  
+ automatically and store it in log files. This information includes   
+internet protocol (IP) addresses, browser type, internet service   
+provider (ISP), referring/exit pages, operating system, date/time stamp,
+   and click stream data. We use this information, which does not 
+identify   individual users, to analyze trends, to administer the site, 
+to track   users’ movements around the site and to gather demographic 
+information   about our user base as a whole. We do not link this   
+automatically-collected data to personally identifiable information.</p>
+        <p><strong>Cookies</strong></p>
+        <p>A cookie is a small text file that is stored on a user’s 
+computer for   record-keeping purposes. We use cookies on this site. We 
+do not link   the information we store in cookies to any personally 
+identifiable   information you submit while on our site. We use session 
+ID cookies. We   use session cookies to make it easier for you to 
+navigate our site. A   session ID cookie expires when you close your 
+browser. We can at your   option set a persistent cookie to store your 
+passwords, so you don’t   have to enter it more than once. Persistent 
+cookies also enable us to   track and target the interests of our users 
+to enhance the experience on   our site. If you reject cookies, you may 
+still use our site, but your   ability to use some areas of our site, 
+such as contests or surveys, will   be limited. Some of our business 
+partners (e.g., advertisers) use   cookies on our site. We have no 
+access to or control over these cookies.   This privacy statement covers
+ the use of our cookies only and does not   cover the use of cookies by 
+any advertisers.</p>
+        <p><strong>Clear Gifs (Web Beacons/Web Bugs)</strong></p>
+        <p>We employ a software technology called clear gifs (a.k.a. Web
+   Beacons/Web Bugs), that help us better manage content on our site by 
+  informing us what content is effective. Clear gifs are tiny graphics  
+ with a unique identifier, similar in function to cookies, and are used 
+  to track the online movements of Web users. In contrast to cookies,   
+which are stored on a user’s computer hard drive, clear gifs are   
+embedded invisibly on Web pages and are about the size of the period at 
+  the end of this sentence. We do not tie the information gathered by   
+clear gifs to our customers’ personally identifiable information. We use
+   clear gifs in our HTML-based emails to let us know which emails have 
+  been opened by recipients. This allows us to gauge the effectiveness 
+of   certain communications and the effectiveness of our marketing 
+campaigns.   If you would like to opt-out of these emails, please see 
+"Choice and   Opt-out."</p>
+        <p><strong>Links to Other Sites</strong></p>
+        <p>This Web site may contain links to other sites such as our 
+affiliates   as well as third parties which are not owned or controlled 
+by us.   Please be aware that this privacy policy only addresses our 
+company’s   use and disclosure of your information collected on this 
+site. While we   try to link only to sites that share our standards and 
+respect for   privacy, we are not responsible for the privacy practices 
+of such other   sites. We encourage you to be aware when you leave our 
+site and to read   the applicable privacy policies and terms of 
+conditions of each and   every Web site that collects personally 
+identifiable information. This   privacy statement applies only to 
+information collected by this Web   site.</p>
+        <p><strong>Co-branded sites</strong></p>
+        <p>We may be co-branded with "partners and affiliates". These 
+business   to business relationships are helpful to us and to you as 
+they afford   all concerned with greater product and service 
+opportunities. We always   provide opt-out opportunities regarding the 
+sharing of your information   with such partners and affiliates. Please 
+note the sites linked to our   site are governed by their own privacy 
+policies which may or may not   reach the standards set by our company.</p>
+        <p>Access to Personally Identifiable Information If your 
+personally   identifiable information changes, or if you no longer 
+desire our product   or service, you may correct, update, delete or 
+deactivate same by   emailing our Customer Support by contacting us by 
+telephone or postal   mail at the contact information listed below.</p>
+        <p><strong>Customer Service</strong></p>
+        <p>If you send us correspondence, including e-mails and faxes, 
+we may   retain such information in your customer file. Information you 
+give us   over the telephone may be noted for your file. We may also 
+keep copies   of any correspondence sent to you. We retain these records
+ in order to   provide the products and services you have requested and 
+to measure and   improve our customer service. We may, over time, delete
+ these records as   permitted by law. Phone calls may be recorded or 
+monitored for customer   satisfaction purposes. Based upon the 
+personally identifiable   information you provide, we may communicate 
+with you to provide the   services you request, and to manage your 
+account. We may communicate via   email or telephone, in accordance with
+ your wishes.</p>
+        <p><strong>Legal Disclaimer</strong></p>
+        <p>We reserve the right to disclose your personally identifiable
+   information, as required, to comply with the law, applicable   
+regulations, governmental and quasi-governmental requests, judicial   
+proceedings, court orders or subpoenas, to enforce our Legal Notices or 
+  other agreements, or to protect our rights, property or safety or the 
+  rights, property or safety of our users or others (e.g., to a consumer
+   reporting agency for fraud protection etc.).</p>
+        <p><strong>Supplementation of Information</strong></p>
+        <p>In order to provide certain services to you, we may on 
+occasion   supplement the personal information you submitted to us with 
+information   from third party sources.</p>
+        <p><strong>Business Transitions</strong></p>
+        <p>In the event our company goes through a business transition, 
+such as a   merger, acquisition by another company, or sale of all or a 
+portion of   its assets, your personally identifiable information will 
+likely be   among the assets transferred. You will be notified via 
+e-mail and/or a   prominent notice on our Web site for 30 days of any 
+such change in   ownership or control of your personal information.</p>
+        <p><strong>Changes to this Privacy Statement</strong></p>
+        <p>We reserve the right to modify this privacy statement at any 
+time. We   will not jeopardize your privacy. The provisions contained in
+ this   privacy statement supersede all previous notices or policies 
+regarding   our privacy practices with respect to this site. Any and all
+ changes   will be made here, to this privacy statement. We encourage 
+you to check   our site frequently to see the current privacy statement 
+to be informed   of how we are committed to protecting your information 
+and providing you   with improved content on our website in order to 
+enhance your online   experience. Upon any material changes to the 
+policy statement, we will   post those changes to this privacy 
+statement, the homepage, and other   places we deem appropriate so that 
+you are aware of what information we   collect, how we use it, and under
+ what circumstances, if any, we   disclose it.</p>
+        <p><strong>Contact Us</strong></p>
+        <p>If you have any questions, comments, complaints or 
+suggestions   regarding our privacy policy or our website, please 
+contact us at   support@debtadvisors22.ca. Mailing address: 4720 Kingsway, Suite 2600 Burnaby, British Columbia, V5H 4N2. </p>
+
+
+      </td>
+
+	</tr>
+	</tbody>
+	</table>
+</center>
+
+
+</body></html>
